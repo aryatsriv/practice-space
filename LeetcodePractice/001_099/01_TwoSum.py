e9921d4472b219f6750eeb98d61a6f0b585796a5
aryat <aryatsrivastava06@gmail.com>
2025-11-09 00:35:18 +0530

    Two sum fix

--- LeetcodePractice/001_099/01_TwoSum.py
+++ LeetcodePractice/001_099/01_TwoSum.py
@@ -38,6 +38,9 @@
 #
 # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
+from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         cache={}
