564cad526bb3024b5ab4d3d5944b72a43474d360
aryat <aryatsrivastava06@gmail.com>
2026-07-22 22:31:55 +0530

    Fixes

---
+++ CSES/01_IntroductoryProblems/23_StringReorder.cpp
@@ -0,0 +1,28 @@
+// String Reorder
+// Your task is to reorder the characters of a string so that no two adjacent
+// characters are the same. What is the lexicographically minimal such string?
+// Input
+// The only line has a string of length n consisting of characters A–Z.
+// Output
+// Print the lexicographically minimal reordered string where no two adjacent
+// characters are the same. If it is not possible to create such a string, print
+// -1. Constraints
+
+// 1 \le n \le 10^6
+
+// Example
+// Input:
+// HATTIVATTI
+
+// Output:
+// AHATITITVT
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  return 0;
+}
